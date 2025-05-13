51eab0a8063a164aa204255b3930ee3189b28bd2
Crazy dev <papaigwe65@gmail.com>
2025-05-13 22:26:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,32 +3,27 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir les fichiers statiques
-app.use(express.static(path.join(__dirname, 'public')));
+// Servir les fichiers statiques depuis la racine
+app.use(express.static(path.join(__dirname)));
 
 // Route pour la page d'accueil
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Route pour la page "start"
 app.get('/start', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'start.html'));
+  res.sendFile(path.join(__dirname, 'start.html'));
 });
 
 // Route pour la page "card"
 app.get('/card/:username', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'card.html'));
+  res.sendFile(path.join(__dirname, 'card.html'));
 });
 
 // Route pour la page "quiz"
 app.get('/quiz', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'quiz.html'));
-});
-
-// Route pour la page "learn"
-app.get('/learn', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'learn.html'));
+  res.sendFile(path.join(__dirname, 'quiz.html'));
 });
 
 // Démarrer le serveur
